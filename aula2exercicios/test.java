98622b96ad2eb4615f31532df9fbcb4b1b37d1e7
Fabioscp <fabioscp@id.uff.br>
2020-09-16 16:32:35 -0300

    Aula2exercicios

---
+++ aula2exercicios/test.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula2exercicios;
+
+/**
+ *
+ * @author fabio
+ */
+public class test {
+    public static void main(String[] args){
+        double x;
+        x = (1.0/2.0)+(2.0/7.0);
+        System.out.printf("%f \n",x);
+    }
+}
